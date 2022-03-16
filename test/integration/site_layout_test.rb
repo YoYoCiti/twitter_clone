93cf6ce52ea468a833631638886a5bdb80c1cb9d
YoYoCiti <xiuwenseow@yahoo.com.sg>
2022-03-16 15:51:13 +0000

    Finish user signup

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,4 +13,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign Up")
   end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash[:success].empty?
+  end
 end
