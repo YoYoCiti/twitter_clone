a2afa8db9ec82278ba5730f47b7ffc29ba1e158e
YoYoCiti <xiuwenseow@yahoo.com.sg>
2022-03-21 13:29:32 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,11 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'https://c4544ac91ceb4765a11f0e4b51897a35.vfs.cloud9.us-east-1.amazonaws.com/'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'https://c4544ac91ceb4765a11f0e4b51897a35.vfs.cloud9.us-east-1.amazonaws.com/' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
